4765e3732b17ae81b9a16cc7f6948561b15a0c85
Jemo <jroberts@students.mags.school.nz>
2026-05-13 12:37:19 +1200

    Echolocation detection working
    The echolocation is being picked up by the tilemap. Now needs to be adjusted so it spread to a wider place.

--- echobat/scripts/bat_player.gd
+++ echobat/scripts/bat_player.gd
@@ -20,13 +20,15 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		soundwave_spawn.scale.x = direction
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if Input.is_action_just_pressed("fire_soundwave"):
-		print("POW")
 		var soundwave = soundwave_scene.instantiate()
 		soundwave.position = soundwave_spawn.global_position
 		soundwave.rotation = $Node2D.rotation
--- echobat/scripts/soundwave.gd
+++ echobat/scripts/soundwave.gd
@@ -2,17 +2,28 @@ extends Node2D
 
 const SPEED = 500
 
+var current_position
+var tilemap: TileMapLayer
+var source_id = 0
+var visible_tile_atlas_id: Vector2i = Vector2i(0, 1)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	tilemap = get_tree().get_first_node_in_group("tilemaps")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_local_x(SPEED * delta)
-	
+	if tilemap:
+		current_position =  tilemap.local_to_map(global_position)
 
 
+# The detection in which I will use echolocation
 func echolocation_detection(body: Node2D) -> void:
-	if body == TileMapLayer:
-		print("TILEMAP")
+	if tilemap:
+		if body == tilemap:
+			var has_tile = tilemap.get_cell_atlas_coords(current_position)
+			if has_tile != Vector2i(-1, -1):
+				tilemap.set_cell(current_position, source_id, visible_tile_atlas_id)
+				queue_free()
