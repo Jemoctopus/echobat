903bf3547a1b53bc316bc0efa2ba0292cdc1bfbf
Jemo <jroberts@students.mags.school.nz>
2026-06-04 09:55:39 +1200

    The terrain fades out but is very laggy

--- echobat/scripts/second_iteration_soundwave.gd
+++ echobat/scripts/second_iteration_soundwave.gd
@@ -4,9 +4,9 @@ extends Node2D
 @export var marker: Node2D
 @export var expiration_timer: Timer
 
-var angle_list = [deg_to_rad(220), deg_to_rad(40)]
+var angle_list = [deg_to_rad(215), deg_to_rad(35)]
 var angle_facing = 30
-var expiration_time = 6
+var expiration_time = 2
 var direction_list = ["left", "right"]
 var direction_facing
 
@@ -14,28 +14,23 @@ var tilemap: TileMapLayer
 var top_soundwave: Node2D
 var bottom_soundwave: Node2D
 
-
 # Terrain Variables
 var terrain_test = {
-	"bright_test" : 0,
-	"fadeout1_test" : 1,
-	"fadeout2_test" : 2,
-	"fadeout3_test" : 3
-	}
-var terrain_set_test = 0
+	"bright" : 0,
+	"fadeout1" : 1,
+	"fadeout2" : 2,
+	"fadeout3" : 3
+} ## Terrain gradient keys
 var terrain_ordering_test  = {
-	0 : "bright_test",
-	1 : "bright_test",
-	2 : "bright_test",
-	3 : "bright_test",
-	4 : "bright_test",
-	5 : "fadeout1_test",
-	6 : "fadeout2_test",
-	7 : "fadeout3_test"
-}## Ordering of the terrain gradient
+	0 : "bright",
+	1 : "fadeout1",
+	2 : "fadeout2",
+	3 : "fadeout3"
+} ## Ordering of the terrain gradient
 var current_terrain_ordering = 0
+var terrain_set_test = 0
 var source_id = 0
-var visible_tile_atlas_id: Vector2i = Vector2i(0, 1)
+var invisible_tile_atlas_id: Vector2i = Vector2i(0, 0)
 
 var go_for: int ## The amount of time the soundwave goes for
 var touching_tiles = false
@@ -46,6 +41,8 @@ var bottom_position
 var current_positions = []
 var previous_positions
 var cells_between = []
+var used_cells = []
+var terrain
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -76,20 +73,36 @@ func _process(delta: float) -> void:
 		top_position =  tilemap.local_to_map(top_soundwave.global_position)
 		bottom_position = tilemap.local_to_map(bottom_soundwave.global_position)
 		current_positions = [top_position, bottom_position]
-		print(current_positions)
 		
 		# Get the cells between the two posistions and then check if the tiles exist.
 		if current_positions != previous_positions:
 			var cell_range = top_position.y - bottom_position.y
-			var used_cells = []
 			for cells_between in range(cell_range):
 				var cell = Vector2i(bottom_position.x, bottom_position.y + cells_between)
 				if not tilemap.get_cell_atlas_coords(cell) == Vector2i(-1, -1):
 					used_cells.append(cell)
-			var terrain = terrain_ordering_test[current_terrain_ordering]
+			terrain = terrain_ordering_test[current_terrain_ordering]
 			tilemap.set_cells_terrain_connect(used_cells, terrain_set_test, terrain_test[terrain], true)
 		previous_positions = current_positions
 
 
 func _on_expiration_timer_timeout() -> void:
-	queue_free()
+	var fadeout1 = []
+	var fadeout2 = []
+	var fadeout3 = []
+	var terrain_data
+	for cells in used_cells:
+		terrain_data = tilemap.get_cell_tile_data(cells)
+		terrain_data = terrain_data.terrain
+		if terrain_data == 0:
+			fadeout1.append(cells)
+		elif terrain_data == 1:
+			fadeout2.append(cells)
+		elif terrain_data == 2:
+			fadeout3.append(cells)
+		elif terrain_data == 3:
+			tilemap.set_cell(cells, source_id, invisible_tile_atlas_id)
+			queue_free()
+	tilemap.set_cells_terrain_connect(fadeout1, terrain_set_test, terrain_test["fadeout1"], true)
+	tilemap.set_cells_terrain_connect(fadeout2, terrain_set_test, terrain_test["fadeout2"], true)
+	tilemap.set_cells_terrain_connect(fadeout3, terrain_set_test, terrain_test["fadeout3"], true)
